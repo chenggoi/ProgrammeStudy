5cb13823f2f984f11eb4b456ca7dd8807289ad93
C.Gong <942824866@qq.com>
2014-08-27 17:29:45 +0800

    LinkList

---
+++ chenggoi/LinkList.cpp
@@ -0,0 +1,110 @@
+/*
+Linklist, include creat, search, insert, delete,display
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct LNode {
+	int data;
+	struct LNode *next;
+} *LinkList, LNode;
+
+LinkList LinkSearch(LinkList L) {
+	int n;
+	printf("Input the position: ");
+	scanf("%d", &n);
+	LinkList p;
+	p = L;
+
+	while(n--) {
+		p = p->next;
+	}
+
+	printf("Result: %d\n", p->data);
+	return p;
+}// LinkSearch
+
+void LinkInsert(LinkList &L) {
+	LinkList p, q;
+	p = (LinkList)malloc(sizeof(LNode));
+	q = (LinkList)malloc(sizeof(LNode));
+	p->next = NULL;
+	q->next = NULL;
+	printf("Inpute the inserted element: ");
+	scanf("%d", &p->data);
+	int n;
+	printf("Input the position: ");
+	scanf("%d", &n);
+	n--;
+	q = L;
+
+	while(n--) {
+		q = q->next;
+	}
+
+	p->next = q->next;
+	q->next = p;
+	printf("Insert success!!\n");
+}// LinkInsert
+
+void LinkDelete(LinkList &L) {
+	LinkList p, q;
+	p = (LinkList)malloc(sizeof(LNode));
+	q = (LinkList)malloc(sizeof(LNode));
+	int n;
+	printf("Input the position: ");
+	scanf("%d", &n);
+	n--;
+	p = L;
+
+	while(n--) {
+		p = p->next;
+	}
+
+	printf("Result: %d is Deleted...\n", p->next->data);
+	q = p->next;
+	p->next = q->next;
+	free(q);
+}// LinkDelete
+
+void LinkDisplay(LinkList L) {
+	L = L->next;
+
+	while(L != NULL) {
+		printf("%d ", L->data);
+		L = L->next;
+	}
+
+	return;
+}
+
+int main()
+{
+	LinkList L, p;
+	L = (LinkList)malloc(sizeof(LNode));
+	L->next = NULL;
+
+	while(1) {
+		printf("Choose the number: 1.Insert 2.Search 3.Delete 4.Display......\n");
+		int n;
+		scanf("%d", &n);
+
+		if(n == 1) {
+			LinkInsert(L);
+		}
+		else if(n == 2) {
+			p = LinkSearch(L);
+		}
+		else if(n == 3) {
+			LinkDelete(L);
+		}
+		else if(n == 4) {
+			LinkDisplay(L);
+		}
+		else {
+			printf("Wrong Input!!!");
+		}
+	}
+
+}
