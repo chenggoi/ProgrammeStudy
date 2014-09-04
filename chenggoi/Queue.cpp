80cd20c28a5fa72729ec3c2cb4ebaceb7a2b5141
C.Gong <942824866@qq.com>
2014-09-04 16:24:26 +0800

    up

--- chenggoi/Queue.cpp
+++ chenggoi/Queue.cpp
@@ -5,45 +5,54 @@ Data structure of linked queue, include create,insert,delete...
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct QNode{
+typedef struct QNode
+{
     int data;
     struct QNode *next;
-}QNode,*QueuePtr;
+} QNode, *QueuePtr;
 
-typedef struct{
+typedef struct
+{
     QueuePtr front; //Queue front pointer
     QueuePtr rear; //Queue rear pointer
-}LinkQueue;
+} LinkQueue;
 
-void QueueCreate(LinkQueue &Q){
-    Q.front=Q.rear=(QueuePtr)malloc(sizeof(QNode));
-    Q.front->next=NULL;
+void QueueCreate(LinkQueue &Q)
+{
+    Q.front = Q.rear = (QueuePtr)malloc(sizeof(QNode));
+    Q.front->next = NULL;
     return;
 }//QueueCreate
 
-void QueueInsert(LinkQueue &Q){
+void QueueInsert(LinkQueue &Q)
+{
     printf("Input the element: ");
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     QueuePtr p;
-    p=(QueuePtr)malloc(sizeof(QNode));
-    p->data=n;
-    p->next=NULL;
-    Q.rear->next=p;
-    Q.rear=p;
+    p = (QueuePtr)malloc(sizeof(QNode));
+    p->data = n;
+    p->next = NULL;
+    Q.rear->next = p;
+    Q.rear = p;
     return;
 }//QueueInsert
 
-void QueueDelete(LinkQueue &Q){
-    if(Q.rear==Q.front) printf("ERROR!\n");
-    else {
+void QueueDelete(LinkQueue &Q)
+{
+    if(Q.rear == Q.front) { printf("ERROR!\n"); }
+    else
+    {
         QueuePtr p;
-        p=Q.front->next;
-        printf("Delete %d !\n",p->data);
-        Q.front->next=p->next;
-        if(Q.rear==p) Q.rear=Q.front;
+        p = Q.front->next;
+        printf("Delete %d !\n", p->data);
+        Q.front->next = p->next;
+
+        if(Q.rear == p) { Q.rear = Q.front; }
+
         free(p);
     }
+
     return;
 }//QueueDeleteQueueDelete
 
@@ -51,14 +60,20 @@ int main()
 {
     LinkQueue Q;
     QueueCreate(Q);
-    while(1){
+
+    while(1)
+    {
         printf("Input the number : 1.Insert 2.Delete.....\n");
         int n;
-        scanf("%d",&n);
-        switch(n) {
-        case 1:QueueInsert(Q);  break;
-        case 2:QueueDelete(Q);  break;
-        default:printf("Wrong input!!\n");
+        scanf("%d", &n);
+
+        switch(n)
+        {
+        case 1: QueueInsert(Q);  break;
+
+        case 2: QueueDelete(Q);  break;
+
+        default: printf("Wrong input!!\n");
         }
     }
 
